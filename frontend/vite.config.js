665450d82b8f8ef4e3839be4ec607ff617722941
YASH GAUTAM <152496325+its-yash0003@users.noreply.github.com>
2025-09-19 11:51:42 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Final-Yr-Project",
+  // base: process.env.VITE_BASE_PATH || "/Final-Yr-Project",
 });
